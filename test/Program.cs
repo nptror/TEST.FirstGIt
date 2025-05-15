90ea24bab009e040d444e263e4e1d8c5bf0db4b5
hwthinhj <127239880+nptror@users.noreply.github.com>
2025-05-15 19:51:12 +0700

    pull one message from vs

--- test/Program.cs
+++ test/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#, My name is Thinh");
 Console.WriteLine("Hello, C#, My name is Thinh");
+string s = "1 message";
